25bedf1f16aec1530037ac773c5c02f98bbd1a79
min-fan <1258162171@qq.com>
2025-04-16 18:26:25 +0800

    fix🐛: build bug

--- app/(dashboard)/home/message/compontents/repost.tsx
+++ app/(dashboard)/home/message/compontents/repost.tsx
@@ -5,13 +5,13 @@ import PostTime from "./post-time";
 export default function Repost() {
   return (
     <div className="space-y-4">
-      <PostUser />
+      {/* <PostUser />
       <PostContent />
       <div className="border border-border rounded-md p-4 bg-foreground">
         <PostUser />
         <PostContent />
       </div>
-      <PostTime />
+      <PostTime /> */}
     </div>
   );
 }
